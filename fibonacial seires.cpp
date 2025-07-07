58a6f5aa3718fc8567c47915a5d1a4c4611e5c0e
si845 <sapasettisiddu@gmail.com>
2025-07-07 13:11:05 +0530

    Add files via upload

---
+++ fibonacial seires.cpp
@@ -0,0 +1,20 @@
+
+
+
+#include <stdio.h>
+int main ()
+{
+    int a=0,b=1,c,n,i,sum=1;
+    printf("Enter no.of elements :");
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
+    {
+        c=a+b;
+        sum=sum+c;
+        a=b;
+        b=c;
+    }
+    printf("The sum of fibinoccai series of %d elements is :%d",n,sum);
+}
+
+
